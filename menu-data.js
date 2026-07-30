88ca0b4d7091f41f625d5b61c451ae6a5a51b662
myteluguhouse-dot <myteluguhouse@gmail.com>
2026-07-30 15:49:07 +0100

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -5,6 +5,7 @@ const menuItems = [
   category: "non-veg",
   ingredients: ["Chicken", "Yogurt", "Spices", "Fried Onion", "Roasted spices"],
   mayContain: ["Dairy"],
+  image: "ChickenBiryani.jpg",
   spice: 3
 }
 ];
